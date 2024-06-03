c73ca9eb2e2f57a09993932843cb96aeb265829a
Aman Kumar <144197075+amank736836@users.noreply.github.com>
2024-06-03 10:16:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/K-Palindrome/kpalindrome.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int n = Integer.parseInt(input_line[0]);
+            int k = Integer.parseInt(input_line[1]);
+            String str = in.readLine();
+
+            Solution ob = new Solution();
+            System.out.println(ob.kPalindrome(str, n, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int kPalindrome(String str, int n, int k) {
+        // code here
+        String rev = new StringBuilder(str).reverse().toString();
+        int x = lcs(str, rev, n); 
+        int min = n-x;
+        return min <= k ? 1 : 0;
+    }
+    
+    public int lcs(String s,String r,int n){
+        int[][] dp = new int[n+1][n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1) == r.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
+        
+    }
+}
