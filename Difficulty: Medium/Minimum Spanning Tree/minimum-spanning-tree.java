22cef4ec2570cf736e90907164338b0d4a65f58f
Aman Kumar <144197075+amank736836@users.noreply.github.com>
2024-07-17 19:10:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -37,27 +37,92 @@ public class Main {
 
 // User function Template for Java
 
+class DisjointSet{
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    
+    public DisjointSet(int n){
+        for(int i=0;i<=n;i++){
+            rank.add(0);
+            parent.add(i);
+            size.add(i);
+        }
+    }
+    
+    public int findParent(int node){
+        if(node == parent.get(node)){
+            return node;
+        }
+        
+        int ulp = findParent(parent.get(node));
+        parent.set(node,ulp);
+        return ulp;
+    }
+    
+    public void unionByRank(int u,int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_u == ulp_v){
+            return;
+        }
+        
+        if(rank.get(ulp_u) < rank.get(ulp_v)){
+            parent.set(ulp_u,ulp_v);
+        }
+        else if(rank.get(ulp_v) < rank.get(ulp_u)){
+            parent.set(ulp_v,ulp_u);
+        }
+        else{
+            parent.set(ulp_u,ulp_v);
+            rank.set(ulp_v,rank.get(ulp_v)+1);
+        }
+    }
+    
+    public void unionBySize(int u,int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if(ulp_u == ulp_v){
+            return;
+        }
+        if(size.get(ulp_u) < size.get(ulp_v)){
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_v) + size.get(ulp_u));
+        }
+        else{
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+
 class Solution {
     static int spanningTree(int V, int E, List<List<int[]>> adj) {
         // Code Here.
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b) -> a[0] - b[0]);
-        boolean visited[] = new boolean[V];
-        Arrays.fill(visited,false);
-        
+        DisjointSet djs = new DisjointSet(V);
+        List<int[]> list = new ArrayList<>();
         int result = 0;
-        pq.add(new int[]{0,0});
+        for(int src=0;src<V;src++){
+            for(int row[] : adj.get(src)){
+                int dest = row[0];
+                int weight = row[1];
+                list.add(new int[]{src,dest,weight});
+            }
+        }
         
-        while(!pq.isEmpty()){
-            int wt = pq.peek()[0];
-            int node = pq.poll()[1];
-            if(visited[node]){
+        Collections.sort(list, (a,b) -> a[2] - b[2]);
+        
+        for(int ele[] : list){
+            int p_u = djs.findParent(ele[0]);
+            int p_v = djs.findParent(ele[1]);
+            
+            if(p_u == p_v){
                 continue;
             }
-            visited[node] = true;
-            result += wt;
-            for(int neighbour[] : adj.get(node)){
-                pq.add(new int[]{neighbour[1],neighbour[0]});
-            }
+            
+            result += ele[2];
+            djs.unionByRank(ele[0],ele[1]);
         }
         
         return result;
