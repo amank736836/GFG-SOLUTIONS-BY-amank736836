7325c6d2eac1148e35afa9ffcc2ac54ec0118bf3
Aman Kumar <144197075+amank736836@users.noreply.github.com>
2024-09-04 21:22:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count ways to N'th Stair(Order does not matter)/count-ways-to-nth-stair-order-does-not-matter.java
+++ Difficulty: Medium/Count ways to N'th Stair(Order does not matter)/count-ways-to-nth-stair-order-does-not-matter.java
@@ -20,8 +20,19 @@ class GFG {
 
 
 class Solution {
+    Integer dp[];
     public int nthStair(int n) {
         // Code here
-        return n/2 + 1;
+        dp = new Integer[n+1];
+        return dfs(n,0);
+    }
+    public int dfs(int n,int index){
+        if(index > n){
+            return 0;
+        }
+        if(dp[index] != null){
+            return dp[index];
+        }
+        return dp[index] = 1 + Math.min(dfs(n,index+1),dfs(n,index+2)); 
     }
 }
